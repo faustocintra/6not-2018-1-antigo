dfaec37bf44ab51365c56919560093f512d0945a
fgcintra <fausto@fgcintra.pro.br>
2018-03-09 19:44:31 -0300

    (09/03) Início da implementação de menus laterais no Ionic

--- Notebook/src/app/app.component.ts
+++ Notebook/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { ProductsPage } from '../pages/products/products';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav : Nav;
+  text : string = '';
+  rootPage: any = TabsPage;
+  pages: Array<{title: string, component: any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +24,24 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.pages = [
+        { title: 'Sobre', component: AboutPage },
+        { title: 'Contato', component: ContactPage},
+        { title: 'Início', component: HomePage},
+        { title: 'Produtos', component: ProductsPage }
+      ];
+
     });
+
   }
+
+  openPage(page : any) {
+    this.nav.setRoot(page.component);
+  }
+
+  rightMenuClick(text : string) {
+    this.text = text;
+  }
+
 }

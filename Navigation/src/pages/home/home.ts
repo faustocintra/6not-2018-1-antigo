5b42316216eebe7175346e36cc776b96fe918bd8
fgcintra <fausto@fgcintra.pro.br>
2018-03-16 20:34:15 -0300

    (16/03) Navegação e passagem de parâmetros entre páginas

--- Navigation/src/pages/home/home.ts
+++ Navigation/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OtherPage } from './../other/other';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public minhaString : string = 'Exemplo 1 - isso é só uma string';
+  public meuJSON : any = { text: ''};
+  otherPage : any = OtherPage;
 
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+  goToOtherPage() {
+    this.navCtrl.push(OtherPage, {text : 'Exemplo 3 - isso é um objeto'});
   }
 
 }

0f5d73df1cf95accf6b77a2b4af293b471689252
Fausto G. Cintra <fausto@fgcintra.pro.br>
2018-03-23 23:39:28 +0000

    (23/03) Criação de component Angular para o Ionic

--- pizzaiolo/src/app/app.module.ts
+++ pizzaiolo/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { OrderFormComponent } from './../components/order-form';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -10,7 +11,8 @@ import { HomePage } from '../pages/home/home';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    OrderFormComponent
   ],
   imports: [
     BrowserModule,
